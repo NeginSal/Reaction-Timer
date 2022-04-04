97b8ea4e8a69c3c63dab31b0ec4a7f5bd35aa9e1
NeginSal <neginsaljooghi@gmail.com>
2022-04-04 11:34:33 +0430

    add components

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Ninja Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" :delay="delay"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Block from './components/Block.vue'
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Block
+  },
+  data() {
+    return {
+      isPlaying: false,
+      display: null,
+    };
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random()*5000
+      this.isPlaying = true;
+    },
+  },
+};
 </script>
 
 <style>
@@ -20,7 +33,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
 }
 </style>
