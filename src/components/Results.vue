079cde83c44f2a26c8fbe397995ea56fd7a033d6
NeginSal <neginsaljooghi@gmail.com>
2022-04-06 10:31:48 +0430

    adding a rank & styles

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,15 +1,35 @@
 <template>
   <div>
-  <p> Reaction Time : {{score}} ms </p>
+    <p>Reaction Time : {{ score }} ms</p>
+    <p class="rank">{{ rank }}</p>
   </div>
 </template>
 
 <script>
 export default {
-props:['score']
-}
+  props: ["score"],
+  data() {
+    return {
+      rank: null,
+    };
+  },
+  mounted() {
+    if (this.score < 250) {
+      this.rank = "Ninja Fingers";
+    } else if (this.score < 400) {
+      this.rank = "Rapid Reflexes";
+    } else {
+      this.rank = "Snail pace ...";
+    }
+  },
+};
 </script>
 
-<style>
+<style scoped>
+.rank {
+  font-size: 1.4em;
+  color: #0faf87;
+  font-weight: bold;
+}
+</style>>
 
-</style>
